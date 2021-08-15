c59d7f662eaff9ae539f8c60f76c1efa35204fba
Orlando T. Hutapea <orlando.timotius.hutapea@gmail.com>
2021-08-15 12:33:44 +0700

    Add method helpers for candidates

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,18 @@ class User < ApplicationRecord
 
   has_one :candidate_attribute, dependent: :destroy
 
+  scope :candidates, -> { joins(:candidate_attribute) }
+
   validates :name, presence: true
   validates :sidn, presence: true, uniqueness: true
+
+  delegate :number, :vision, :mission, to: :candidate_attribute, allow_nil: true
+
+  def candidate?
+    !candidate_attribute.nil?
+  end
+
+  def can_vote?
+    !candidate?
+  end
 end
