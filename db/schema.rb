6e9d760dc7501d95a456ed441caa7a7bd663031b
Dousea <orlando.timotius.hutapea@gmail.com>
2021-08-16 18:03:28 +0700

    Update DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 2021_08_15_104959) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "sidn"
-    t.integer "voted_candidate"
     t.integer "vote_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
