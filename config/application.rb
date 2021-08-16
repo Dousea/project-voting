a0299561b041d0ade4844c262a1ee0a7f456cd18
Dousea <orlando.timotius.hutapea@gmail.com>
2021-08-17 03:14:45 +0700

    Add SMTP settings for mailer

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,15 @@ module ProjectVoting
     config.time_zone = 'Jakarta'
     config.i18n.available_locales = %i[en id]
     config.i18n.default_locale = :id
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: ENV['SMTP_ADDRESS'],
+      port: ENV['SMTP_PORT'],
+      domain: ENV['SMTP_DOMAIN'],
+      user_name: ENV['SMTP_USERNAME'],
+      password: ENV['SMTP_PASSWORD'],
+      authentication: ENV['SMTP_AUTHENTICATION'].to_sym,
+      enable_starttls_auto: true
+    }
   end
 end
