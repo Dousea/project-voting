b7c999dc1d26da2b2a6dcbed0839175dd2e38385
Dousea <orlando.timotius.hutapea@gmail.com>
2021-08-17 05:25:29 +0700

    Use HTTPS protocol for email links

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "project_voting_production"
 
-  config.action_mailer.default_url_options = { host: 'evotif.online' }
+  config.action_mailer.default_url_options = { host: 'evotif.online', protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
